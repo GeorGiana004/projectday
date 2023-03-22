691bea55f0fccaa932f017e3dff58e3e5ee9f3de
GeorGiana004 <123818805+GeorGiana004@users.noreply.github.com>
2023-03-22 18:04:29 +0100

    primo commit

--- src/pagest/Home.jsx
+++ src/pagest/Home.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 
+
 const Home =()=>{
     return(
-        <h1>Home </h1>
+        
+
+   
+<body className="container ">
+    im
+    
+
+       
+        <div className="home-container1">
+         
+            <h1 className="title" >Sicurezza Informatica </h1>
+
+                <h1 className="title2" >Perche dobbiamo prottegerci? </h1>
+                    <p className="description1">descrizine del Sperche dobbiamo farlo</p>
+                    
+      
+         <h1 className="title3" >Come proteggersi </h1>
+                    <p className="description2"></p>
+        
+        </div>
+    
+      </body>  
+       
     )
 }; 
 export default Home; 
